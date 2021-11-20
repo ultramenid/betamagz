1168dda9495970076a8de808d49fe90f1593cd13
ultramenid <malichamdan@gmail.com>
2021-11-21 03:02:55 +0700

    page loader

--- js/app.js
+++ js/app.js
@@ -1,54 +1,62 @@
+$('#content').hide();
+    
+    $(window).on('load', function(){
+      $('#loader').hide();
+      $('#content').show();
+      const intro = document.querySelector("#intro");
+      const intromobile = document.querySelector("#intromobile");
+      const video = intro.querySelector("video");
+      const videomobile = document.getElementById("videomobile");
+      const text = intro.querySelector("h1");
+      //END SECTION
+      const section = document.querySelector("#section");
+      const end = section.querySelector("h1");
+
+      //SCROLLMAGIC
+      const controller = new ScrollMagic.Controller();
+
+      //Scenes
+      let scene = new ScrollMagic.Scene({
+        duration: 17000,
+        triggerElement: intro,
+        triggerHook: 0
+      })
+        // .addIndicators()
+        .setPin(intro)
+        .addTo(controller);
+
+        //Scenes
+      let scenemobile = new ScrollMagic.Scene({
+        duration: 1000,
+        triggerElement: intromobile,
+        triggerHook: 0
+      })
+        // .addIndicators()
+        .setPin(intromobile)
+        .addTo(controller);
+
+      //Video Animation
+      let accelamount = 1;
+      let scrollpos = 0;
+      let delay = 0;
+
+      scene.on("update", e => {
+        scrollpos = e.scrollPos / 900;
+      });
+
+      scenemobile.on("enter", e => {
+        videomobile.muted = true;
+        videomobile.playsinline = true;
+        videomobile.play();
+      });
 
 
-const intro = document.querySelector("#intro");
-const intromobile = document.querySelector("#intromobile");
-const video = intro.querySelector("video");
-const videomobile = document.getElementById("videomobile");
-const text = intro.querySelector("h1");
-//END SECTION
-const section = document.querySelector("#section");
-const end = section.querySelector("h1");
+      setInterval(() => {
+        delay += (scrollpos - delay)  + 0.01;
+        video.currentTime = delay;
+      }, 80);
 
-//SCROLLMAGIC
-const controller = new ScrollMagic.Controller();
-
-//Scenes
-let scene = new ScrollMagic.Scene({
-  duration: 17000,
-  triggerElement: intro,
-  triggerHook: 0
-})
-  // .addIndicators()
-  .setPin(intro)
-  .addTo(controller);
-
-  //Scenes
-let scenemobile = new ScrollMagic.Scene({
-  duration: 2000,
-  triggerElement: intromobile,
-  triggerHook: 0
-})
-  // .addIndicators()
-  .setPin(intromobile)
-  .addTo(controller);
-
-//Video Animation
-let accelamount = 1;
-let scrollpos = 0;
-let delay = 0;
-
-scene.on("update", e => {
-  scrollpos = e.scrollPos / 900;
-});
-
-scenemobile.on("enter", e => {
-  videomobile.muted = true;
-  videomobile.playsinline = true;
-  videomobile.play();
-});
-
-
-setInterval(() => {
-  delay += (scrollpos - delay)  + 0.01;
-  video.currentTime = delay;
-}, 80);
+    });
+    $(document).ready(function(e) {
+      $('img[usemap]').rwdImageMaps();
+    });
